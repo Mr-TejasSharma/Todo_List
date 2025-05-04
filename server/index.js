37c7d331945916f3702e74bb519d4d5e3dab4604
Mr-TejasSharma <tejas.sharma111002@gmail.com>
2025-05-05 01:24:03 +0530

    bcryptjs

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const User = require("./models/users");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 app.use(express.json());
